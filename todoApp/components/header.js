93b0043dcf07f9725bc938874b51d5f7b10f28d1
Marcián <marcianszlovak@gmail.com>
2020-01-05 13:01:44 +0100

    header component

---
+++ todoApp/components/header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function header(params) {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.title}>What to do</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    height: 85,
+    paddingTop: 40,
+    backgroundColor: "beige"
+  }
+});
