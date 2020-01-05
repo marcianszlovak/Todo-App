d6f24bbb2591c7ccaccaf64c607351de9fae1c45
Marcián <marcianszlovak@gmail.com>
2020-01-05 12:46:11 +0100

    view styles

--- todoApp/App.js
+++ todoApp/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+  return;
+  <View style={styles.container}>
+    {/* header */}
+    <View style={styles.content}>
+      {/* to do form */}
+      <View style={styles.list}></View>
     </View>
-  );
+  </View>;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
