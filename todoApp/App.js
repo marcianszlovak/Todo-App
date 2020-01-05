a40b46404e9c9429c4f1d04e76baf2d6c4eec627
Marcián <marcianszlovak@gmail.com>
2020-01-05 21:02:18 +0100

    alerts

--- todoApp/App.js
+++ todoApp/App.js
@@ -28,8 +28,9 @@ export default function App() {
 
   const submitHandler = text => {
     if (text.length > 3) {
+      setText("");
       setTodos(prevTodos => {
-        return [{ text: text, key: Math.random().toString() }, ...prevTodos];
+        return [{ text, key: Math.random().toString() }, ...prevTodos];
       });
     } else {
       Alert.alert("Todos must be over 3 characters long.", [
@@ -39,45 +40,41 @@ export default function App() {
         }
       ]);
     }
-
-    return (
-      <TouchableWithoutFeedback
-        onPress={() => {
-          Keyboard.dismiss();
-        }}
-      >
-        <View style={styles.container}>
-          <Header />
-          <View style={styles.content}>
-            <addToList submitHandler={submitHandler} />
-            <View style={styles.list}>
-              <FlatList
-                data={todos}
-                renderItem={({ item }) => (
-                  <todoItem item={item} pressHandler={pressHandler} />
-                )}
-              />
-            </View>
-          </View>
-        </View>
-      </TouchableWithoutFeedback>
-    );
   };
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#fff",
-      alignItems: "center",
-      justifyContent: "center"
-    },
-    content: {
-      padding: 40,
-      flex: 1
-    },
-    list: {
-      marginTop: 20,
-      flex: 1
-    }
-  });
+  return (
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler} />
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                <TodoItem item={item} pressHandler={pressHandler} />
+              )}
+            />
+          </View>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  content: {
+    padding: 40,
+    flex: 1
+  },
+  list: {
+    marginTop: 20,
+    flex: 1
+  }
+});
--- todoApp/components/header.js
+++ todoApp/components/header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function header(params) {
+export default function header() {
   return (
     <View style={styles.header}>
       <Text style={styles.title}>What to do</Text>
