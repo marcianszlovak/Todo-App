e8bc7eafb7df1e0cd85d0b52b08b1a9bdb40d895
Marcián <marcianszlovak@gmail.com>
2020-01-05 13:55:49 +0100

    add new items to the list

---
+++ todoApp/components/addToList.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, TextInput, Button, View } from "react-native";
+
+export default function addToList() {
+  const [text, setText] = useState(""); //keeps track of what the user types into the input field
+  const changeHandler = value => {
+    setText(value);
+  };
+
+  return (
+    <View>
+      <TextInput
+        style={styles.input}
+        placeholder="new item..."
+        onChangeText={changeHandler}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  input: {
+    marginBottom: 12,
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ddd"
+  }
+});
