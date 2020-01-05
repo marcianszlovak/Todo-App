bfdc46f107f756384e16951dbb3f44c4993a46df
Marcián <marcianszlovak@gmail.com>
2020-01-05 12:49:24 +0100

    todo list

--- todoApp/App.js
+++ todoApp/App.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, FlatList } from "react-native";
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { text: "do something", key: "1" },
+    { text: "do something else", key: "2" },
+    { text: "don't do that", key: "3" }
+  ]);
+
   return;
   <View style={styles.container}>
     {/* header */}
     <View style={styles.content}>
       {/* to do form */}
       <View style={styles.list}></View>
+      <FlatList />
     </View>
   </View>;
 }
