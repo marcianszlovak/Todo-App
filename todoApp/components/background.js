0cdf4d46c9836e573ff38845daf603e2f36114e6
Marcián Szlovák <saintvicious11@gmail.com>
2020-01-18 10:56:21 +0100

    background

---
+++ todoApp/components/background.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, Text, Button, Image, ImageBackground } from "react-native";
+
+export default function Background() {
+  // return()
+}
+
+const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    opacity: 0.7
+  }
+});
